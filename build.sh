a2b74cba9e6d5de250ab6f535809a3477ae8d61c
Steve Pieper <pieper@isomics.com>
2017-02-06 14:09:31 -0500

    Build slicer-chronicle without caching
    Allows it to always have the most up to date
    git checkout.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker build -t stevepieper/base base
 docker build -t stevepieper/x11 x11
 docker build -t stevepieper/slicer slicer
 
-docker build -t stevepieper/slicer-chronicle slicer-chronicle
+docker build --no-cache -t stevepieper/slicer-chronicle slicer-chronicle
