e1d56d1feddd96eada25195baf59cec3b4ecb4ec
Steve Pieper <pieper@isomics.com>
2017-07-16 15:27:37 -0400

    Add a slicer-dev continer

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 
 docker build -t stevepieper/base base
 docker build -t stevepieper/x11 x11
+docker build -t stevepieper/slicer-dev slicer-dev
 docker build -t stevepieper/slicer3 slicer3
 docker build -t stevepieper/slicer slicer
 
---
+++ slicer-dev/README.md
@@ -0,0 +1,5 @@
+[Slicer](https://www.slicer.org/) Docker image with dev dependecies for building Slicer (Qt5)
+
+Run with:
+
+docker run -d -p 8080:8080 --name slicer-dev stevepieper/slicer-dev
